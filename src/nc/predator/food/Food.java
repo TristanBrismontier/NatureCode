81262130adb5dc25bbb53ea5fe8b7486fe15b729
Tristan Brismontier <tristan.brismontier@gmail.com>
2014-09-17 13:44:11 +0200

    Update Food.java

--- src/nc/predator/food/Food.java
+++ src/nc/predator/food/Food.java
@@ -7,38 +7,30 @@ import nc.NoiseP;
 
 public class Food extends Mover {
 
-	float tx=0;
-	float ty=10000;
-	
 	public Food(PApplet p, float m, boolean d3) {
 		super(p, m, d3);
-
 	}
 
 	public void display2D(){
 		p.stroke(0);
 		p.fill(5, 0, 200, 125);
-		
 		p.ellipse(location.x, location.y, mass*5, mass*5);
 	}
 	
 	public void interac(Mover other) {
 		if (other instanceof Predator) {
+			//attract devrais définir un Vector vitess plustôt qu'un vector acceleration
 			PVector force = other.attract(this);
 			PVector f = PVector.div(force, mass);
 			acceleration.add(f);
 		}
-			
 	}
 	
 	@Override
 	public void update() {
-		tx ++;
-		ty ++;
 		PVector randomPVector = PVector.random2D();
 		randomPVector.mult(p.random(1));
 		acceleration.add(randomPVector);
-		System.out.println(acceleration.x + " | " + acceleration.y);
 		super.update();
 	}
 	
