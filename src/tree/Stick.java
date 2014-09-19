e2343c49fb59fb5dc05215f7b4874a2ba442289c
Tristan Brismontier <tristan.brismontier@gmail.com>
2014-09-19 13:05:16 +0200

    Update Stick.java

--- src/tree/Stick.java
+++ src/tree/Stick.java
@@ -1,22 +1,18 @@
 package tree;
 import java.util.ArrayList;
 import java.util.List;
-
 import processing.core.PApplet;
 import processing.core.PVector;
 
-
 public class Stick extends EntityT {
-
-	boolean parentStick;
 	PVector velocity;
 	List<Stick> sticks;
+	boolean parentStick;
 	float vampireLife ;
 	float life;
 
-
 	public Stick(final PApplet parent, PVector location, float width, float life, PVector velocity){
-		super(parent, null, location, width	, width);
+		super(parent, null, location, width, width);
 		sticks = new ArrayList<Stick>();
 		this.life = life;	
 		this.velocity = velocity;
@@ -24,7 +20,7 @@ public class Stick extends EntityT {
 	}
 
 	public Stick(final PApplet parent, PVector location, int width, float life){
-		super(parent, null, location, width	, width);
+		super(parent, null, location, width, width);
 		sticks = new ArrayList<Stick>();
 		this.life = life;	
 		this.velocity = new PVector(0,(float)0.05,0);
@@ -48,6 +44,7 @@ public class Stick extends EntityT {
 	private void computeNewData() {
 		location.add(velocity);		
 		life -=(float)0.15;
+		//add map instead
 		velocity.y -= ( parent.random(-(200/((parentStick)?2:1)),200) /50000);
 		float xrand = (parentStick)?200:350;
 		velocity.x += ((velocity.x >1)||(velocity.x <-1))? 0 :( parent.random(-xrand,xrand) /10000);
@@ -59,6 +56,7 @@ public class Stick extends EntityT {
 		float ratioWidth = (float) ((parentStick)?0.75:0.9);
 		PVector newLocation =  new PVector();
 		newLocation= location.get();
+		//add map instead
 		PVector newVelocity = new PVector(velocity.x+(parent.random(-1,1)*(float)0.3), velocity.y+(parent.random(-1,1)*(float)0.1),0);
 		sticks.add(new Stick(parent, newLocation, width*(float)ratioWidth, life*(float)ratiolife, newVelocity));
 		vampireLife -= 1.2;
