b3368d6a7425fcea3758738c47dc9b3391e39916
Tristan Brismontier <tristan.brismontier@gmail.com>
2014-09-19 12:57:05 +0200

    Update EntityT.java

--- src/tree/EntityT.java
+++ src/tree/EntityT.java
@@ -1,33 +1,27 @@
 package tree;
-
-
-
-
 import processing.core.PApplet;
 import processing.core.PVector;
 
-
 public class EntityT {
 
-	final PApplet parent;
+	final PApplet p;
 	final ColorT color;
 	PVector location;
 	float width;
 	float height;
 
-
-	public EntityT(PApplet parent, ColorT color, PVector location, float width,
+	public EntityT(PApplet p, ColorT color, PVector location, float width,
 			float height) {
 		super();
-		this.parent = parent;
+		this.p = p;
 		this.color = color;
 		this.location = location;
 		this.width = width;
 		this.height = height;
 	}
 
-	public PApplet getParent() {
-		return parent;
+	public PApplet getp() {
+		return p;
 	}
 	public ColorT getColor() {
 		return color;
