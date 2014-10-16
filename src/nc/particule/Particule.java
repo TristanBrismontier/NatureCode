bb18e1152bf60d3ec86edf1f9f1e95a91cde497c
Tristan Brismontier <dev.tristan.brismontier@gmail.com>
2014-10-16 20:40:25 +0200

    Particule

--- src/nc/particule/Particule.java
+++ src/nc/particule/Particule.java
@@ -10,10 +10,21 @@ public class Particule {
 	PVector acceleration;
 	float wieght;
 	
-	public Particule(PApplet p) {
-		super();
+	public Particule(PApplet p,PVector location) {
+		this.location = location;
 		this.p = p;
+		this.wieght = 10; 
 	}
 		
+	public void update() {
+		location.add(velocite);
+		velocite.add(acceleration);
+	}
+	
+	public void display(){
+		p.stroke(0);
+		p.fill(175);
+		p.ellipse(location.x, location.y,wieght,wieght);
+	}
 
 }
