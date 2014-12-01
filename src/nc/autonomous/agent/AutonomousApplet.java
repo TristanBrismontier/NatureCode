6f0226bced0f82d7f8161bb76084f900edae48dd
Tristan Brismontier <dev.tristan.brismontier@gmail.com>
2014-12-01 19:49:34 +0100

    autonomous agent

---
+++ src/nc/autonomous/agent/AutonomousApplet.java
@@ -0,0 +1,15 @@
+package nc.autonomous.agent;
+
+import processing.core.PApplet;
+
+public class AutonomousApplet extends PApplet {
+	
+	public void setup() {
+		size(800, 800);
+	}
+	
+	public void draw() {
+		
+	}
+	
+}
