13e7600a3777e05c71169c3ea373f0149715f3da
Tristan Brismontier <tristan.brismontier@gmail.com>
2014-12-07 16:21:57 +0100

    Game of life

--- src/nc/cellular/automata/CellularPApplet.java
+++ src/nc/cellular/automata/CellularPApplet.java
@@ -8,31 +8,29 @@ import processing.core.PApplet;
 
 public class CellularPApplet extends PApplet {
 
+	
 	List<List<Boolean>> cells;
 	int size;
-	boolean[] ruleset = {false,true,false,true,true,false,true,false};
 	
 	public void setup() {
 		size(810, 800);
-		size = 2;
-		initCells(false);
+		size = 20;
+//		frameRate(2);
+		initCells();
 	}
 
-	private void initCells(boolean randomRule) {
-		if(randomRule){
-			Random random = new Random();
-			for (int i = 0; i < ruleset.length; i++) {
-				ruleset[i] = random.nextBoolean();
-			}
-		}
+	private void initCells() {
 		cells = new ArrayList<>();
+		for (int i = 0; i < height / size; i++) {
+			initCollumn();
+		}
+	}
+
+	private void initCollumn() {
 		List<Boolean> first = new ArrayList<>();
+		Random ran = new Random();
 		for (int i = 0; i < width / size; i++) {
-			if (i == (int) (width / size / 2)) {
-				first.add(true);
-			} else {
-				first.add(false);
-			}
+			first.add(ran.nextBoolean());
 		}
 		cells.add(first);
 	}
@@ -42,11 +40,11 @@ public class CellularPApplet extends PApplet {
 			return;
 		}
 		System.out.println(frameCount);
-		background(0);
 		for (int i = 0; i < cells.size(); i++) {
 			List<Boolean> currentCells = cells.get(i);
 			for (int j = 0; j < currentCells.size(); j++) {
 				Boolean current = currentCells.get(j);
+				int liveNeightboor = computeLive(i,j);
 				if (current) {
 					fill(255);
 				} else {
@@ -54,44 +52,72 @@ public class CellularPApplet extends PApplet {
 				}
 				noStroke();
 				rect(j * size, i * size, size, size);
+				currentCells.set(j, computeCell(current,liveNeightboor));
 			}
 		}
+	}
+	
 
-		List<Boolean> currentCells = cells.get(cells.size() - 1);
-		List<Boolean> nextCells = new ArrayList<>();
-		for (int j = 0; j < currentCells.size(); j++) {
-			nextCells.add(applyRules(j, currentCells));
+	private Boolean computeCell(Boolean current, int liveNeightboor) {
+		if(current){
+			if(liveNeightboor >=4 || liveNeightboor <= 1){
+				return false;
+			} else {
+				return true;
+			}
+		}else{
+			if(liveNeightboor == 3){
+				return true;
+			}else{
+				return false;
+			}
 		}
-		cells.add(nextCells);
+		
 	}
 
-	private Boolean applyRules(int j, List<Boolean> currentCells) {
-		Boolean next = false;
-		Boolean previous = false;
+	private int computeLive(int i, int j) {
+		int liveNeightboor = 0;
+		if (i - 1 > 0) {
+			liveNeightboor += computeLiveCell(j, cells.get(i-1), true);
+		}else{
+			liveNeightboor +=5;
+		}
+		if (i + 1 < cells.size()) {
+			liveNeightboor += computeLiveCell(j, cells.get(i+1), true);
+		}else{
+			liveNeightboor +=5;
+		}
+		liveNeightboor += computeLiveCell(j, cells.get(i), false);
+		
+		return liveNeightboor;
+	}
+
+	private int computeLiveCell(int j, List<Boolean> currentCells, boolean computeCurrent) {
+		int liveNeightboor = 0;
 		if (j - 1 > 0) {
-			previous = currentCells.get(j - 1);
+			liveNeightboor += (currentCells.get(j - 1))?1:0;
+		}else{
+			liveNeightboor +=5;
 		}
 		if (j + 1 < currentCells.size()) {
-			next = currentCells.get(j + 1);
+			liveNeightboor += (currentCells.get(j + 1))?1:0;
+		}else{
+			liveNeightboor +=5;
 		}
-		return computeRule(previous, currentCells.get(j), next);
+		
+		if(computeCurrent){
+			liveNeightboor +=currentCells.get(j)?1:0;
+		}
+		return liveNeightboor;
 	}
 
 	private Boolean computeRule(boolean previous, boolean current, boolean next) {
-		if      (previous && current && next) return ruleset[0];
-	    else if (previous && current && !next) return ruleset[1];
-	    else if (previous && !current && next) return ruleset[2];
-	    else if (previous && !current && !next) return ruleset[3];
-	    else if (!previous && current && next) return ruleset[4];
-	    else if (!previous && current && !next) return ruleset[5];
-	    else if (!previous && !current && next) return ruleset[6];
-	    else if (!previous && !current && !next) return ruleset[7];
-	    return false;
+		return false;
 	}
 	
 	@Override
 	public void keyPressed() {
-		initCells(true);
+		initCells();
 	}
 	
 	
