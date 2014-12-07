8ab702b492b14053424b80988541e7656317f3af
Tristan Brismontier <dev.tristan.brismontier@gmail.com>
2014-12-07 13:47:42 +0100

    Sierpiński triangle

--- src/nc/cellular/automata/CellularPApplet.java
+++ src/nc/cellular/automata/CellularPApplet.java
@@ -6,37 +6,76 @@ import java.util.List;
 import processing.core.PApplet;
 
 public class CellularPApplet extends PApplet {
-	
-	List<Boolean> cells;
+
+	List<List<Boolean>> cells;
 	int size;
+	boolean[] ruleset = {false,true,false,true,true,false,true,false};
 	
 	public void setup() {
 		size(810, 800);
-		size = 10;
-		cells = new ArrayList<Boolean>();
-		for (int i = 0; i < width/size; i++) {
-			if(i == (int)(width/size/2)){
-				cells.add(true);
-			}else{
-				cells.add(false);
-			}
-		}
-		
-	}
-	
-	public void draw() {
-		background(0);
 
-		for (int j = 0; j < cells.size(); j++) {
-			if(cells.get(j)){
-				fill(255);
-			}else{
-				fill(0);
+		size = 5;
+		cells = new ArrayList<>();
+		List<Boolean> first = new ArrayList<>();
+		for (int i = 0; i < width / size; i++) {
+			if (i == (int) (width / size / 2)) {
+				first.add(true);
+			} else {
+				first.add(false);
 			}
-			stroke(255);
-			rect(j*10,height/2,10,10);
 		}
-		
-		
+		cells.add(first);
+	}
+
+	public void draw() {
+		if(cells.size()*size>height){
+			return;
+		}
+		System.out.println(frameCount);
+		background(0);
+		for (int i = 0; i < cells.size(); i++) {
+			List<Boolean> currentCells = cells.get(i);
+			for (int j = 0; j < currentCells.size(); j++) {
+				Boolean current = currentCells.get(j);
+				if (current) {
+					fill(255);
+				} else {
+					fill(0);
+				}
+				noStroke();
+				rect(j * size, i * size, size, size);
+			}
+		}
+
+		List<Boolean> currentCells = cells.get(cells.size() - 1);
+		List<Boolean> nextCells = new ArrayList<>();
+		for (int j = 0; j < currentCells.size(); j++) {
+			nextCells.add(applyRules(j, currentCells));
+		}
+		cells.add(nextCells);
+	}
+
+	private Boolean applyRules(int j, List<Boolean> currentCells) {
+		Boolean next = false;
+		Boolean previous = false;
+		if (j - 1 > 0) {
+			previous = currentCells.get(j - 1);
+		}
+		if (j + 1 < currentCells.size()) {
+			next = currentCells.get(j + 1);
+		}
+		return computeRule(previous, currentCells.get(j), next);
+	}
+
+	private Boolean computeRule(boolean previous, boolean current, boolean next) {
+		if      (previous && current && next) return ruleset[0];
+	    else if (previous && current && !next) return ruleset[1];
+	    else if (previous && !current && next) return ruleset[2];
+	    else if (previous && !current && !next) return ruleset[3];
+	    else if (!previous && current && next) return ruleset[4];
+	    else if (!previous && current && !next) return ruleset[5];
+	    else if (!previous && !current && next) return ruleset[6];
+	    else if (!previous && !current && !next) return ruleset[7];
+	    return false;
 	}
 }
