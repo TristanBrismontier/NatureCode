a36d47e9020c2ca10645fb5685687dc97158ea44
Tristan Brismontier <dev.tristan.brismontier@gmail.com>
2014-12-07 16:59:48 +0100

    Game of life with init

--- src/nc/cellular/automata/CellularPApplet.java
+++ src/nc/cellular/automata/CellularPApplet.java
@@ -8,13 +8,13 @@ import processing.core.PApplet;
 
 public class CellularPApplet extends PApplet {
 
-	
 	List<List<Cell>> cells;
 	int size;
-	
+	boolean simulate = false;
+
 	public void setup() {
-		size(200, 200);
-		size = 5;
+		size(500, 500);
+		size = 10;
 		frameRate(5);
 		initCells();
 	}
@@ -30,89 +30,98 @@ public class CellularPApplet extends PApplet {
 		List<Cell> first = new ArrayList<>();
 		Random ran = new Random();
 		for (int i = 0; i < width / size; i++) {
-			first.add(new Cell(ran.nextBoolean(),0));
+			first.add(new Cell(false, 0));
 		}
 		cells.add(first);
 	}
 
 	public void draw() {
-		if(cells.size()*size>height){
-			return;
-		}
-		System.out.println(frameCount);
+
 		for (int i = 0; i < cells.size(); i++) {
 			List<Cell> currentCells = cells.get(i);
 			for (int j = 0; j < currentCells.size(); j++) {
 				Cell current = currentCells.get(j);
-				current.setLiveNeigbour(computeLive(i,j));
+				current.setLiveNeigbour(computeLive(i, j));
 				if (current.isALive()) {
 					fill(255);
 				} else {
 					fill(0);
 				}
 				noStroke();
+				if(!simulate){
+					stroke(255);
+				}
 				rect(j * size, i * size, size, size);
 			}
 		}
-		
-		for (List<Cell> cellss : cells) {
-			for (Cell cell : cellss) {
-				cell.setLive(computeCell(cell.isALive(), cell.liveNeigbour));
+		if (simulate) {
+			for (List<Cell> cellss : cells) {
+				for (Cell cell : cellss) {
+					cell.setLive(computeCell(cell.isALive(), cell.liveNeigbour));
+				}
 			}
 		}
-		
+
 	}
-	
+
+	@Override
+	public void mouseClicked() {
+		super.mouseClicked();
+		Cell cell = cells.get((int)(mouseY/size)).get((int)(mouseX/size));
+		cell.setLive(!cell.isALive());
+
+	}
 
 	private Boolean computeCell(Boolean current, int liveNeightboor) {
-		if(current){
-			if(liveNeightboor >=4 || liveNeightboor <= 1){
+		if (current) {
+			if (liveNeightboor >= 4 || liveNeightboor <= 1) {
 				return false;
 			} else {
 				return true;
 			}
-		}else{
-			if(liveNeightboor == 3){
+		} else {
+			if (liveNeightboor == 3) {
 				return true;
-			}else{
+			} else {
 				return false;
 			}
 		}
-		
+
 	}
 
 	private int computeLive(int i, int j) {
 		int liveNeightboor = 0;
 		if (i - 1 > 0) {
-			liveNeightboor += computeLiveCell(j, cells.get(i-1), true);
-		}else{
-			liveNeightboor +=5;
+			liveNeightboor += computeLiveCell(j, cells.get(i - 1), true);
+		} else {
+			liveNeightboor += 5;
 		}
 		if (i + 1 < cells.size()) {
-			liveNeightboor += computeLiveCell(j, cells.get(i+1), true);
-		}else{
-			liveNeightboor +=5;
+			liveNeightboor += computeLiveCell(j, cells.get(i + 1), true);
+		} else {
+			liveNeightboor += 5;
 		}
 		liveNeightboor += computeLiveCell(j, cells.get(i), false);
-		
+
 		return liveNeightboor;
 	}
 
-	private int computeLiveCell(int j, List<Cell> currentCells, boolean computeCurrent) {
+	private int computeLiveCell(int j, List<Cell> currentCells,
+			boolean computeCurrent) {
 		int liveNeightboor = 0;
 		if (j - 1 > 0) {
-			liveNeightboor += (currentCells.get(j - 1).isALive())?1:0;
-		}else{
-			liveNeightboor +=5;
+			liveNeightboor += (currentCells.get(j - 1).isALive()) ? 1 : 0;
+		} else {
+			liveNeightboor += 5;
 		}
 		if (j + 1 < currentCells.size()) {
-			liveNeightboor += (currentCells.get(j + 1).isALive())?1:0;
-		}else{
-			liveNeightboor +=5;
+			liveNeightboor += (currentCells.get(j + 1).isALive()) ? 1 : 0;
+		} else {
+			liveNeightboor += 5;
 		}
-		
-		if(computeCurrent){
-			liveNeightboor +=currentCells.get(j).isALive()?1:0;
+
+		if (computeCurrent) {
+			liveNeightboor += currentCells.get(j).isALive() ? 1 : 0;
 		}
 		return liveNeightboor;
 	}
@@ -120,11 +129,10 @@ public class CellularPApplet extends PApplet {
 	private Boolean computeRule(boolean previous, boolean current, boolean next) {
 		return false;
 	}
-	
+
 	@Override
 	public void keyPressed() {
-		initCells();
+		simulate = !simulate;
 	}
-	
-	
+
 }
